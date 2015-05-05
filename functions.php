92367d6524fe7fbecf76f624631cc6862695017c
Lars Eidam <larseidam@googlemail.com>
2015-05-05 14:18:28 +0200

    multiple changes
    - add commandline parameter
    - add help commandline string
    - add regex check for betrag, iban and bic properties
    - add function to generate sepa file
    - finsh sepa file creation
    - add different exit codes

--- functions.php
+++ functions.php
@@ -1,13 +1,107 @@
 <?php
+    require_once 'vendor/autoload.php';
+    use Digitick\Sepa\TransferFile\CustomerCreditTransferFile;
+    use Digitick\Sepa\TransferInformation\CustomerCreditTransferInformation;
+    use Digitick\Sepa\GroupHeader;
+    use Digitick\Sepa\PaymentInformation;
+
     /*
      * Funktion um die korrekten Inhalte der Überweisungsmappen-Spalte zu filtern
+     * $var String Überweisungsmappen-Bezeichner
      */
     function correctMapStr($var)
     {
         global $config;
-        if (preg_match($config['Allgemein']['mapregex'], $var))
+        if (1 == preg_match($config['Kontrollregex']['mapregex'], $var))
             return true;
         else
             return false;
+    }
+
+    /*
+     * Funktion erzeugt Sepa File
+     * @param $currentMap String Name des zubearbeitenten Überweisungs-Bezeichner
+     * @param $currentMapRows Array von den zubearbeitenten Zeilen aus der Excel-Tabelle
+     * @param $neededColumnNumbers Array von den notwendigen Spalten aus der Excel-Tabelle
+     * @return CustomerCreditTransferFile Sepa-File-Handler mit den Überweisungen
+     */
+    function getSepaFile($currentMap, $currentMapRows, $neededColumnNumbers)
+    {
+        global $config;
+        global $messages;
+
+        // Erstellung der initzialen Überweisungsparameter
+        $groupHeader = new GroupHeader('SEPA File Identifier', $config['Importkonto']['firmenname']);
+        $sepaFile = new CustomerCreditTransferFile($groupHeader);
+
+        // Zahlungsinformationen (Sender) erstellen
+        $paymentInformation = new PaymentInformation(
+            $currentMap, // Payment Info ID
+            $config['Importkonto']['iban'], // IBAN the money is transferred from
+            $config['Importkonto']['bic'], // BIC
+            $config['Importkonto']['name'] // Debitor Name
+        );
+
+        foreach ($currentMapRows as $currentMapRowNumber => $currentMapRow) {
+            // einzelnen Spalten auf Vorhandensein prüfen
+            $betrag = isset($neededColumnNumbers['betrag']) ? number_format($currentMapRow[$neededColumnNumbers['betrag']], 2, ".", "") : "";
+            $iban = isset($neededColumnNumbers['iban']) ? $currentMapRow[$neededColumnNumbers['iban']] : "";
+            $name = isset($neededColumnNumbers['name']) ? $currentMapRow[$neededColumnNumbers['name']] : "";
+            $bic = isset($neededColumnNumbers['bic']) ? $currentMapRow[$neededColumnNumbers['bic']] : "";
+            $zweck1 = isset($neededColumnNumbers['zweck1']) ? $currentMapRow[$neededColumnNumbers['zweck1']] : "";
+            $zweck2 = isset($neededColumnNumbers['zweck2']) ? $currentMapRow[$neededColumnNumbers['zweck2']] : "";
+            $zweck = "" != $zweck2 ? $zweck1 . " " . $zweck2 : $zweck1;
+
+            if (1 != preg_match($config['Kontrollregex']['betragregex'], $betrag)){
+                $messages[] = "Zeile " . ($currentMapRowNumber + 1) . ": " . $betrag . " ist kein gültiger Betrag und wird deswegen nicht übernommen!";
+                $betrag = "";
+            }
+            $iban = str_replace(" ", "", $iban);
+            if (1 != preg_match($config['Kontrollregex']['ibanregex'], $iban)){
+                $messages[] = "Zeile " . ($currentMapRowNumber + 1) . ": " . $iban . " ist kein gültiger IBAN und wird deswegen nicht übernommen!";
+                $iban = "";
+            }
+            $bic = str_replace(" ", "", $bic);
+            if (1 != preg_match($config['Kontrollregex']['bicregex'], $bic)){
+                $messages[] = "Zeile " . ($currentMapRowNumber + 1) . ": " . $bic . " ist kein gültiger BIC und wird deswegen nicht übernommen!";
+                $bic = "";
+            }
+
+            // Zahlungsinformationen (Empfänger) anlegen
+            $transfer = new CustomerCreditTransferInformation(
+                $betrag, // Amount
+                $iban, //IBAN of creditor
+                $name //Name of Creditor
+            );
+            $transfer->setBic($bic); // Set the BIC explicitly
+            $transfer->setRemittanceInformation($zweck);
+
+            // Zahlungsinformationen zusammenfügen
+            $paymentInformation->addTransfer($transfer);
+        }
+
+        // Zahlungsinformationen zur SEPA-Datei hinzufügen
+        $sepaFile->addPaymentInformation($paymentInformation);
+
+        return $sepaFile;
+    }
+
+    /**
+     * Erzeugt Konsolen-Hilftext
+     */
+    function generateHelpStr()
+    {
+        return "Script zur Erzeugung einer SEPA-XML-Datei aus einer Excel-Tabelle." . PHP_EOL
+        . "Aufruf:" . PHP_EOL
+        . "\tphp main.php [OPTION …]" . PHP_EOL
+        . PHP_EOL
+        . "Hilfeoptionen:" . PHP_EOL
+        . "-h, --help\t\t\tHilfeoptionen anzeigen" . PHP_EOL
+        . PHP_EOL
+        . "Anwendungsoptionen:" . PHP_EOL
+        . "-f, --force\t\t\tÜberschreiben vorhandener Dateien erzwingen." . PHP_EOL
+        . "-l, --list\t\t\tListe der verfügbaren Überweisungsmappen-Bezeichner ausgeben." . PHP_EOL
+        . "-m <Bezeichner>," . PHP_EOL
+        . " --map=<Bezeichner>\t\tSEPA-Datei für bestimmten Überweisungsmappen-Bezeichner erstellen. (z.B.: UEM-2015-05-02)" . PHP_EOL;
     }
 ?>
--- main.php
+++ main.php
@@ -1,11 +1,35 @@
 <?php
+/**
+ * Created by Lars Eidam.
+ * User: Lars Eidam
+ * Date: 05.05.15
+ * Time: 14:02
+ *
+ * Exit-Codes:
+ *  -1 Keine MAP angegeben.
+ *  -2 MAP-Bezeichner nicht in Excel-Tabelle enthalten.
+ *  -3 XML Datei schon vorhanden.
+ */
     include 'vendor/simplexlsx/simplexlsx.class.php';
     include 'functions.php';
 
-    header("Content-Type: text/html; charset=utf-8");
+    require_once 'vendor/autoload.php';
+    use Digitick\Sepa\DomBuilder\DomBuilderFactory;
 
     // Konfiguration einlesen
     $config = parse_ini_file("config.ini", true);
+
+    // Array mit Ausgabe Nachrichten
+    $messages = array();
+
+    // Array mit Übergabeparametern
+    $optionArray = getopt("fhlm::", array("force", "help", "list", "map::"));
+
+    // Konsolen-Hilfstext ausgeben
+    if (isset($optionArray["h"]) || isset($optionArray["help"])) {
+        echo(generateHelpStr());
+        exit;
+    }
 
     // einlesen der Excel-Tabelle
     $xlsx = new SimpleXLSX($config['Allgemein']['tabellenpfad']);
@@ -13,30 +37,75 @@
     $headlineRow = $xlsx->rowsEx();
     $headlineRow = $headlineRow[$config['Allgemein']['namenszeile'] - 1];
 
-    // Ermittlung der Splate für die Überweisungsmappen
-    $mapCloumn = array_search($config['Allgemein']['mapspaltenname'], array_column($headlineRow, 'value'));
+    // Ermittlung der Spaltenummer für die Überweisungsmappen
+    $mapColumnNumber = array_search($config['Allgemein']['mapspaltenname'], array_column($headlineRow, 'value'));
 
     // Ermittlung welche verschiedenen Arten von Überweisungsmappen-Bezeichnungen existieren
-    $mapsAvailable = array_filter(array_unique(array_column($xlsx->rows(), $mapCloumn)), "correctMapStr");
+    $mapsAvailable = array_filter(array_unique(array_column($xlsx->rows(), $mapColumnNumber)), "correctMapStr");
     // Überweisungsmappen-Bezeichnungen sortieren und invertieren damit, aktuellste Bezeichnung ganz oben steht
     sort ($mapsAvailable);
     $mapsAvailable = array_reverse($mapsAvailable);
 
-    $currentMap = $mapsAvailable[0];
+    if (isset($optionArray["l"]) || isset($optionArray["list"])) {
+        echo(implode($mapsAvailable, PHP_EOL));
+        exit;
+    }
+    if (isset($optionArray["m"]) || isset($optionArray["map"])) {
+        $currentMap = "";
+        if (isset($optionArray["m"]) && false !== $optionArray["m"]) {
+            $currentMap = $optionArray["m"];
+        } elseif(isset($optionArray["map"]) && false !== $optionArray["map"]) {
+            $currentMap = $optionArray["map"];
+        } else {
+            echo "Sie müssen eine Map angeben!" .PHP_EOL;
+            exit -1;
+        }
+        if (false === array_search($currentMap, $mapsAvailable)) {
+            echo "Überweisungsmappen-Bezeichner " . $currentMap . " ist in Excel-Tabelle nicht vorhanden!" .PHP_EOL;
+            echo "Folgende Bezeichner sind vorhanden:" .PHP_EOL;
+            echo(implode($mapsAvailable, PHP_EOL));
+            exit -2;
+        }
+        // alle Zeilennummern einer bestimmten Überweisungsmappen-Bezeichnung ermitteln
+        $currentMapRowNumbers = array_keys(array_column($xlsx->rows(), $mapColumnNumber), $currentMap);
+        // alle Zeilen einer bestimmten Überweisungsmappen-Bezeichnung ermitteln
+        $currentMapRows = array_intersect_key($xlsx->rows(), array_flip($currentMapRowNumbers));
 
-    // alle Zeilen einer bestimmten Überweisungsmappen-Bezeichnung ermitteln
-    $currentMapRows = array_keys(array_column($xlsx->rows(), $mapCloumn), $currentMap);
+        // Array für alle notwendigen Splatennumern und mit Überweisungs-Bezeichner als Schlüssel
+        $neededColumnNumbers = array();
+        foreach ($config['Spaltenamen'] as $bankTransferName => $columnName) {
+            if ("" != $columnName && false != array_search($columnName, array_column($headlineRow, 'value'))) {
+                // Ermittlung der Spalte für die Überweisungsmappen
+                $neededColumnNumbers[$bankTransferName] = array_search($columnName, array_column($headlineRow, 'value'));
+            }
+        }
+        // SEPA-Datei mit den Überweisungen aus der Excel-Tabelle erzeugen lassen
+        $sepaFile = getSepaFile($currentMap, $currentMapRows, $neededColumnNumbers);
 
+        // DomBuilder erzeugen um mit desen Hilfe aus der SEPA-Datei eine XML Datei zu generieren
+        $sepaFileDomBuilder = DomBuilderFactory::createDomBuilder($sepaFile);
 
-    echo '<h1>$xlsx->rows()</h1>';
-    echo '<pre>';
-    //var_dump($config['Allgemein']['tabellenpfad']);
-    var_dump($currentMapRows);
-    //print_r( $xlsx->rows() );
-    echo '</pre>';
+        if (file_exists ( $config['Allgemein']['sepapfad'] . $currentMap . ".xml" )
+            && false == isset($optionArray["f"])
+            && false ==  isset($optionArray["force"])) {
+            echo "Die Datei " . $currentMap . ".xml ist schon vorhanden." . PHP_EOL . "Zum überschreiben bitte Option -f oder --force benutzen.";
+            exit -3;
+        } else {
+            // XML-Datei erzeugen, mit der Form 'UEM-2015-05-02.xml'
+            $sepaXMLFileHandle = fopen($config['Allgemein']['sepapfad'] . $currentMap . ".xml", "w");
+            fwrite($sepaXMLFileHandle, $sepaFileDomBuilder->asXml());
+            fclose($sepaXMLFileHandle);
 
-    echo '<h1>$xlsx->rowsEx()</h1>';
-    echo '<pre>';
-    //print_r( $xlsx->rowsEx() );
-    echo '</pre>';
+            // XML-Datei erzeugen, mit der Form 'UEM-2015-05-02.xml'
+            $messageFileHandle = fopen($config['Allgemein']['logpfad'] . $currentMap . "_" . date("Y.m.d-H:i:s") . ".log", "w");
+            fwrite($messageFileHandle, implode($messages, PHP_EOL));
+            fclose($messageFileHandle);
+        }
+    }
+    if (0 != count($messages)) {
+        echo "Folgende Probleme sind während der Erstellung der SEPA-XML-Datei aufgetreten:" .PHP_EOL . "  ";
+        echo implode($messages, PHP_EOL . "  ");
+        echo PHP_EOL;
+    }
+    exit;
 ?>
