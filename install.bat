30e4b53ba852447498ff7b04c4a1e92becaf296c
Lars Eidam <larseidam@googlemail.com>
2015-06-03 17:02:04 +0200

    remove windows installation file and powershell link

--- install.bat
+++
@@ -1,6 +0,0 @@
-REM PHP Composer runterladen und installieren
-php-binary\php.exe -r "readfile('https://getcomposer.org/installer');" | php
-php-binary\php.exe composer.phar install
-
-REM Konfigurationsdatei anlegen
-copy config.ini.dist config.ini
